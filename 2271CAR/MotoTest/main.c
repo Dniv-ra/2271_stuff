46c52da2d16a24fbfd4cff89ef78af1662552d96
Sridharan Arvind Srinivasan <arv1920@gmail.com>
2022-03-29 03:28:49 +0800

    PWM rewrite turn gud now hopefully

--- 2271CAR/MotoTest/main.c
+++ 2271CAR/MotoTest/main.c
@@ -18,49 +18,11 @@
 #define MOTOR_FRONT_LEFT_FWD 29
 #define MOTOR_FRONT_LEFT_BK  30
 #define MOTOR_FRONT_RIGHT_FWD  4
-#define MOTOR_FRONT_RIGHT_BK   5
+#define MOTOR_FRONT_RIGHT_BK   5 
 #define BAUD_RATE 9600
- 
-void InitGPIO(void)
-{
-	// Enable Clock to PORTA
-	SIM->SCGC5 |= ((SIM_SCGC5_PORTA_MASK) | (SIM_SCGC5_PORTE_MASK));
-	// Configure MUX settings to make all 3 pins GPIO
-	//BACK
-	PORTA->PCR[MOTOR_BACK_LEFT_BK] &= ~PORT_PCR_MUX_MASK;
-	PORTA->PCR[MOTOR_BACK_LEFT_BK] |= PORT_PCR_MUX(1);
-	
-	PORTA->PCR[MOTOR_BACK_LEFT_FWD] &= ~PORT_PCR_MUX_MASK;
-	PORTA->PCR[MOTOR_BACK_LEFT_FWD] |= PORT_PCR_MUX(1);
-	
-	PORTA->PCR[MOTOR_BACK_RIGHT_BK] &= ~PORT_PCR_MUX_MASK;
-	PORTA->PCR[MOTOR_BACK_RIGHT_BK] |= PORT_PCR_MUX(1);
-	
-	PORTA->PCR[MOTOR_BACK_RIGHT_FWD] &= ~PORT_PCR_MUX_MASK;
-	PORTA->PCR[MOTOR_BACK_RIGHT_FWD] |= PORT_PCR_MUX(1);
-	
-	//FRONT
-	PORTE->PCR[MOTOR_FRONT_LEFT_FWD] &= ~PORT_PCR_MUX_MASK;
-	PORTE->PCR[MOTOR_FRONT_LEFT_FWD] |= PORT_PCR_MUX(1);
-	
-	PORTE->PCR[MOTOR_FRONT_LEFT_BK] &= ~PORT_PCR_MUX_MASK;
-	PORTE->PCR[MOTOR_FRONT_LEFT_BK] |= PORT_PCR_MUX(1);
-	
-	PORTE->PCR[MOTOR_FRONT_RIGHT_FWD] &= ~PORT_PCR_MUX_MASK;
-	PORTE->PCR[MOTOR_FRONT_RIGHT_FWD] |= PORT_PCR_MUX(1);
-	
-	PORTE->PCR[MOTOR_FRONT_RIGHT_BK] &= ~PORT_PCR_MUX_MASK;
-	PORTE->PCR[MOTOR_FRONT_RIGHT_BK] |= PORT_PCR_MUX(1);
-		
-	// Set Data Direction Registers for PortB and PortD
-	PTA->PDDR |= (MASK(MOTOR_BACK_LEFT_BK) | MASK(MOTOR_BACK_LEFT_FWD) | MASK(MOTOR_BACK_RIGHT_BK) | MASK(MOTOR_BACK_RIGHT_FWD));
-	PTE->PDDR |= (MASK(MOTOR_FRONT_LEFT_FWD) | MASK(MOTOR_FRONT_LEFT_BK) | MASK(MOTOR_FRONT_RIGHT_FWD) | MASK(MOTOR_FRONT_RIGHT_BK));
-}
 
 volatile uint8_t data_to_send;
 volatile uint8_t received_data;
-//Unsure should double check
-//volatile int new_data = 0;
 
 void UART2_IRQHandler(void) 
 {
@@ -73,8 +35,6 @@ void UART2_IRQHandler(void)
 	else
 	{
 		 received_data = UART2_D;
-		 //Has received new data 
-		 //new_data = 1;
 	}
 }
 
@@ -110,12 +70,44 @@ void initUART(uint32_t baud)
 }
 
 
+void InitGPIOMotor(void)
+{
+	// Enable Clock to PORTA
+	SIM->SCGC5 |= ((SIM_SCGC5_PORTA_MASK) | (SIM_SCGC5_PORTE_MASK));
+	// Configure MUX settings to make all 3 pins GPIO
+	//BACK
+	PORTA->PCR[MOTOR_BACK_LEFT_BK] &= ~PORT_PCR_MUX_MASK;
+	PORTA->PCR[MOTOR_BACK_LEFT_BK] |= PORT_PCR_MUX(1); //Change to the TPM setting
+	
+	PORTA->PCR[MOTOR_BACK_LEFT_FWD] &= ~PORT_PCR_MUX_MASK;
+	PORTA->PCR[MOTOR_BACK_LEFT_FWD] |= PORT_PCR_MUX(1); //Change to the TPM setting
+	
+	PORTA->PCR[MOTOR_BACK_RIGHT_BK] &= ~PORT_PCR_MUX_MASK;
+	PORTA->PCR[MOTOR_BACK_RIGHT_BK] |= PORT_PCR_MUX(1); //Change to the TPM setting
+	
+	PORTA->PCR[MOTOR_BACK_RIGHT_FWD] &= ~PORT_PCR_MUX_MASK;
+	PORTA->PCR[MOTOR_BACK_RIGHT_FWD] |= PORT_PCR_MUX(1); //Change to the TPM setting
+	
+	//FRONT
+	PORTE->PCR[MOTOR_FRONT_LEFT_FWD] &= ~PORT_PCR_MUX_MASK;
+	PORTE->PCR[MOTOR_FRONT_LEFT_FWD] |= PORT_PCR_MUX(1); //Change to the TPM setting
+	
+	PORTE->PCR[MOTOR_FRONT_LEFT_BK] &= ~PORT_PCR_MUX_MASK;
+	PORTE->PCR[MOTOR_FRONT_LEFT_BK] |= PORT_PCR_MUX(1); //Change to the TPM setting
+	
+	PORTE->PCR[MOTOR_FRONT_RIGHT_FWD] &= ~PORT_PCR_MUX_MASK;
+	PORTE->PCR[MOTOR_FRONT_RIGHT_FWD] |= PORT_PCR_MUX(1); //Change to the TPM setting
+	
+	PORTE->PCR[MOTOR_FRONT_RIGHT_BK] &= ~PORT_PCR_MUX_MASK;
+	PORTE->PCR[MOTOR_FRONT_RIGHT_BK] |= PORT_PCR_MUX(1); //Change to the TPM setting
+}
+
+
 volatile int counter = 0;
 //PIT IRQ here
 void PIT_IRQHandler() {
 	counter++;
 }
-
 
 void initPIT(void) {
 	PIT_LDVAL1 = 47; // setup timer 1 for 47 cycles (cycles/sec * time = value) (clkspd is 48000000 ->  48 cycles is 10^-6 s, 1 microsecond)
@@ -128,7 +120,7 @@ void initPIT(void) {
 volatile int ultraFlag = 0;
 osSemaphoreId_t ultraSem;
 
-void PORTA_IRQHandler(void) //change A to whatever port the echo pin is supposed to be, enable interrupts
+void PORTX_IRQHandler(void) //change X to whatever port the echo pin is supposed to be, enable interrupts
 {
 	
 	if(!ultraFlag){
@@ -147,13 +139,79 @@ void PORTA_IRQHandler(void) //change A to whatever port the echo pin is supposed
 		//release the semaphore
 		osSemaphoreRelease(ultraSem);
 	}
+	//PORTX_ISFR = 0xFFFFFFFF; //Clear flag (change to whatever port)
+}
+
+int calc_mod(int clkspd, int prescaler, int req_freq) {
+	int cycles_per_sec = clkspd / prescaler;
+	return (cycles_per_sec / req_freq) - 1;
+}
+
+int calc_cnv(int mod, float req_duty) {
+	return (mod * req_duty);
+}
+
+void initPWM(void) {
+	//The mode setting is done in initGPIOMotor
+	SIM->SCGC6 |= SIM_SCGC6_TPM1_MASK;
+	//CLOCK SETUP
+	SIM->SOPT2 &= ~SIM_SOPT2_TPMSRC_MASK; //Clearing the bits in TPMSRC
+	//Use MCGFLLCLK CLK
+	SIM->SOPT2 |= SIM_SOPT2_TPMSRC(1); 
+	
+	//For now setting as TPM1 CH0,l and TPM0 CH0,1 can change later based on pins
+	//TPM0
+	TPM0->SC &= ~((TPM_SC_CMOD_MASK) | (TPM_SC_PS_MASK));
+	TPM0->SC |= (TPM_SC_CMOD(1) | TPM_SC_PS(7)); // Prescaler = 2^7 = 128
+	TPM0->SC &= ~TPM_SC_CPWMS_MASK;
+	
+	TPM0_C0SC &= ~(TPM_CnSC_ELSB_MASK | TPM_CnSC_ELSA_MASK | TPM_CnSC_MSA_MASK | TPM_CnSC_MSB_MASK);
+	TPM0_C0SC |= (TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1));
+	
+	TPM0_C1SC &= ~(TPM_CnSC_ELSB_MASK | TPM_CnSC_ELSA_MASK | TPM_CnSC_MSA_MASK | TPM_CnSC_MSB_MASK);
+	TPM0_C1SC |= (TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1));
+	
+	//TPM1
+	TPM1->SC &= ~((TPM_SC_CMOD_MASK) | (TPM_SC_PS_MASK));
+	TPM1->SC |= (TPM_SC_CMOD(1) | TPM_SC_PS(7)); // Prescaler = 2^7 = 128
+	TPM1->SC &= ~TPM_SC_CPWMS_MASK;
+	
+	TPM1_C0SC &= ~(TPM_CnSC_ELSB_MASK | TPM_CnSC_ELSA_MASK | TPM_CnSC_MSA_MASK | TPM_CnSC_MSB_MASK);
+	TPM1_C0SC |= (TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1));
+	
+	TPM1_C1SC &= ~(TPM_CnSC_ELSB_MASK | TPM_CnSC_ELSA_MASK | TPM_CnSC_MSA_MASK | TPM_CnSC_MSB_MASK);
+	TPM1_C1SC |= (TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1));
+	
+	//Set the pwm to 0 for init
+	TPM1_MOD = calc_mod(48000000, 128, 262);
+	TPM1_C0V = calc_cnv(TPM1_MOD, 0);
+	TPM1_C1V = calc_cnv(TPM1_MOD, 0);
+	
+	TPM0_MOD = calc_mod(48000000, 128, 262);
+	TPM0_C0V = calc_cnv(TPM0_MOD, 0);
+	TPM0_C1V = calc_cnv(TPM0_MOD, 0);
+}
+
+void initUltrasound(void) {
+	//Dunno the current pins
+	//SIM->SCGC5 |= SIM_SCGC5_PORTX_MASK (change the X to required port)
+	//PORTX->PCR[ECHO] &= ~PORT_PCR_MUX_MASK;
+	//PORTX->PCR[ECHO] |= PORT_PCR_MUX(1); 
+	//PORTX->PCR[TRIGGER] &= ~PORT_PCR_MUX_MASK;
+	//PORTX->PCR[TRIGGER] |= PORT_PCR_MUX(1);
+	//PTA->PDDR |= MASK(TRIGGER); //Trigger is output
+	//PTA->PDDR &= ~MASK(ECHO); //Echo is input
+	
+	//Interrupts for echo pin
+	//PORTX->PCR[ECHO] |= PORT_PCR_IRQC(0b1011); //1011 is either edge
+	//NVIC_ClearPendingIRQ(PORTX_IRQn);
+	//NVIC_SetPriority(PORTX_IRQn, 2);
+	//NVIC_EnableIRQ(PORTX_IRQn);
 }
 
 /*----------------------------------------------------------------------------
  * Threads
  *---------------------------------------------------------------------------*/
-
-
 int distance = 0;
 void ultrasonic (void *argument) {
 	for(;;) {
@@ -176,59 +234,67 @@ void app_main (void *argument) {
   for (;;) {
 		switch(received_data) {
 			case 0x31: //forward
-				PTA->PSOR |= MASK(MOTOR_BACK_LEFT_FWD);
-				PTA->PCOR |= MASK(MOTOR_BACK_LEFT_BK);
-				PTA->PSOR |= MASK(MOTOR_BACK_RIGHT_FWD);
-				PTA->PCOR |= MASK(MOTOR_BACK_RIGHT_BK);
-			
-				PTE->PSOR |= MASK(MOTOR_FRONT_LEFT_FWD);
-				PTE->PCOR |= MASK(MOTOR_FRONT_LEFT_BK);
-				PTE->PSOR |= MASK(MOTOR_FRONT_RIGHT_FWD);
-				PTE->PCOR |= MASK(MOTOR_FRONT_RIGHT_BK);
+				//we can have two motors attached to one signal so makes life easier (frt left and back left to same TPM1 ch0, ch1) 
+				TPM1_C0V = calc_cnv(TPM1_MOD, 1); //MOTOR_FRONT_LEFT_FWD and MOTOR_BACK_LEFT_FWD
+				TPM1_C1V = calc_cnv(TPM1_MOD, 0); //MOTOR_FRONT_LEFT_BK and MOTOR_BACK_LEFT_BK
+				TPM0_C0V = calc_cnv(TPM0_MOD, 1); //MOTOR_FRONT_RIGHT_FWD and MOTOR_BACK_RIGHT_FWD
+				TPM0_C1V = calc_cnv(TPM0_MOD, 0); //MOTOR_FRONT_RIGHT_BK and MOTOR_BACK_RIGHT_BK
 				break;
+			
 			case 0x32: //back
-				PTA->PSOR |= MASK(MOTOR_BACK_LEFT_BK);
-				PTA->PCOR |= MASK(MOTOR_BACK_LEFT_FWD);
-				PTA->PSOR |= MASK(MOTOR_BACK_RIGHT_BK);
-				PTA->PCOR |= MASK(MOTOR_BACK_RIGHT_FWD);
-			
-				PTE->PSOR |= MASK(MOTOR_FRONT_LEFT_BK);
-				PTE->PCOR |= MASK(MOTOR_FRONT_LEFT_FWD);
-				PTE->PSOR |= MASK(MOTOR_FRONT_RIGHT_BK);
-				PTE->PCOR |= MASK(MOTOR_FRONT_RIGHT_FWD);
+				TPM1_C0V = calc_cnv(TPM1_MOD, 0); //MOTOR_FRONT_LEFT_FWD and MOTOR_BACK_LEFT_FWD
+				TPM1_C1V = calc_cnv(TPM1_MOD, 1); //MOTOR_FRONT_LEFT_BK and MOTOR_BACK_LEFT_BK
+				TPM0_C0V = calc_cnv(TPM0_MOD, 0); //MOTOR_FRONT_RIGHT_FWD and MOTOR_BACK_RIGHT_FWD
+				TPM0_C1V = calc_cnv(TPM0_MOD, 1); //MOTOR_FRONT_RIGHT_BK and MOTOR_BACK_RIGHT_BK
 				break;
-			case 0x33: //left
-				PTA->PSOR |= MASK(MOTOR_BACK_LEFT_BK);
-				PTA->PCOR |= MASK(MOTOR_BACK_LEFT_FWD);
-				PTA->PSOR |= MASK(MOTOR_BACK_RIGHT_FWD);
-				PTA->PCOR |= MASK(MOTOR_BACK_RIGHT_BK);
 			
-				PTE->PSOR |= MASK(MOTOR_FRONT_LEFT_BK);
-				PTE->PCOR |= MASK(MOTOR_FRONT_LEFT_FWD);
-				PTE->PSOR |= MASK(MOTOR_FRONT_RIGHT_FWD);
-				PTE->PCOR |= MASK(MOTOR_FRONT_RIGHT_BK);
+			case 0x33: //left forward curved
+				TPM1_C0V = calc_cnv(TPM1_MOD, 0.5); //MOTOR_FRONT_LEFT_FWD and MOTOR_BACK_LEFT_FWD
+				TPM1_C1V = calc_cnv(TPM1_MOD, 0); //MOTOR_FRONT_LEFT_BK and MOTOR_BACK_LEFT_BK
+				TPM0_C0V = calc_cnv(TPM0_MOD, 1); //MOTOR_FRONT_RIGHT_FWD and MOTOR_BACK_RIGHT_FWD
+				TPM0_C1V = calc_cnv(TPM0_MOD, 0); //MOTOR_FRONT_RIGHT_BK and MOTOR_BACK_RIGHT_BK
 				break;
-			case 0x34: //right
-				PTA->PSOR |= MASK(MOTOR_BACK_LEFT_FWD);
-				PTA->PCOR |= MASK(MOTOR_BACK_LEFT_BK);
-				PTA->PSOR |= MASK(MOTOR_BACK_RIGHT_BK);
-				PTA->PCOR |= MASK(MOTOR_BACK_RIGHT_FWD);
 			
-				PTE->PSOR |= MASK(MOTOR_FRONT_LEFT_FWD);
-				PTE->PCOR |= MASK(MOTOR_FRONT_LEFT_BK);
-				PTE->PSOR |= MASK(MOTOR_FRONT_RIGHT_BK);
-				PTE->PCOR |= MASK(MOTOR_FRONT_RIGHT_FWD);
+			case 0x34: //right forward curved
+				TPM1_C0V = calc_cnv(TPM1_MOD, 1); //MOTOR_FRONT_LEFT_FWD and MOTOR_BACK_LEFT_FWD
+				TPM1_C1V = calc_cnv(TPM1_MOD, 0); //MOTOR_FRONT_LEFT_BK and MOTOR_BACK_LEFT_BK
+				TPM0_C0V = calc_cnv(TPM0_MOD, 0.5); //MOTOR_FRONT_RIGHT_FWD and MOTOR_BACK_RIGHT_FWD
+				TPM0_C1V = calc_cnv(TPM0_MOD, 0); //MOTOR_FRONT_RIGHT_BK and MOTOR_BACK_RIGHT_BK
 				break;
-			case 0x35:
-				PTA->PCOR |= MASK(MOTOR_BACK_LEFT_BK);
-				PTA->PCOR |= MASK(MOTOR_BACK_LEFT_FWD);
-				PTA->PCOR |= MASK(MOTOR_BACK_RIGHT_BK);
-				PTA->PCOR |= MASK(MOTOR_BACK_RIGHT_FWD);
 			
-				PTE->PCOR |= MASK(MOTOR_FRONT_LEFT_FWD);
-				PTE->PCOR |= MASK(MOTOR_FRONT_LEFT_BK);
-				PTE->PCOR |= MASK(MOTOR_FRONT_RIGHT_FWD);
-				PTE->PCOR |= MASK(MOTOR_FRONT_RIGHT_BK);
+			case 0x36: //left forward curved
+				TPM1_C0V = calc_cnv(TPM1_MOD, 0); //MOTOR_FRONT_LEFT_FWD and MOTOR_BACK_LEFT_FWD
+				TPM1_C1V = calc_cnv(TPM1_MOD, 0.5); //MOTOR_FRONT_LEFT_BK and MOTOR_BACK_LEFT_BK
+				TPM0_C0V = calc_cnv(TPM0_MOD, 0); //MOTOR_FRONT_RIGHT_FWD and MOTOR_BACK_RIGHT_FWD
+				TPM0_C1V = calc_cnv(TPM0_MOD, 1); //MOTOR_FRONT_RIGHT_BK and MOTOR_BACK_RIGHT_BK
+				break;
+			
+			case 0x37: //right backward curved
+				TPM1_C0V = calc_cnv(TPM1_MOD, 0); //MOTOR_FRONT_LEFT_FWD and MOTOR_BACK_LEFT_FWD
+				TPM1_C1V = calc_cnv(TPM1_MOD, 1); //MOTOR_FRONT_LEFT_BK and MOTOR_BACK_LEFT_BK
+				TPM0_C0V = calc_cnv(TPM0_MOD, 0); //MOTOR_FRONT_RIGHT_FWD and MOTOR_BACK_RIGHT_FWD
+				TPM0_C1V = calc_cnv(TPM0_MOD, 0.5); //MOTOR_FRONT_RIGHT_BK and MOTOR_BACK_RIGHT_BK
+				break;
+			
+			case 0x38: //left strict
+				TPM1_C0V = calc_cnv(TPM1_MOD, 0); //MOTOR_FRONT_LEFT_FWD and MOTOR_BACK_LEFT_FWD
+				TPM1_C1V = calc_cnv(TPM1_MOD, 1); //MOTOR_FRONT_LEFT_BK and MOTOR_BACK_LEFT_BK
+				TPM0_C0V = calc_cnv(TPM0_MOD, 1); //MOTOR_FRONT_RIGHT_FWD and MOTOR_BACK_RIGHT_FWD
+				TPM0_C1V = calc_cnv(TPM0_MOD, 0); //MOTOR_FRONT_RIGHT_BK and MOTOR_BACK_RIGHT_BK
+				break;
+			
+			case 0x39: //right strict
+				TPM1_C0V = calc_cnv(TPM1_MOD, 1); //MOTOR_FRONT_LEFT_FWD and MOTOR_BACK_LEFT_FWD
+				TPM1_C1V = calc_cnv(TPM1_MOD, 0); //MOTOR_FRONT_LEFT_BK and MOTOR_BACK_LEFT_BK
+				TPM0_C0V = calc_cnv(TPM0_MOD, 0); //MOTOR_FRONT_RIGHT_FWD and MOTOR_BACK_RIGHT_FWD
+				TPM0_C1V = calc_cnv(TPM0_MOD, 1); //MOTOR_FRONT_RIGHT_BK and MOTOR_BACK_RIGHT_BK
+				break;
+			
+			case 0x35: //stop
+				TPM1_C0V = calc_cnv(TPM1_MOD, 0); //MOTOR_FRONT_LEFT_FWD and MOTOR_BACK_LEFT_FWD
+				TPM1_C1V = calc_cnv(TPM1_MOD, 0); //MOTOR_FRONT_LEFT_BK and MOTOR_BACK_LEFT_BK
+				TPM0_C0V = calc_cnv(TPM0_MOD, 0); //MOTOR_FRONT_RIGHT_FWD and MOTOR_BACK_RIGHT_FWD
+				TPM0_C1V = calc_cnv(TPM0_MOD, 0); //MOTOR_FRONT_RIGHT_BK and MOTOR_BACK_RIGHT_BK
 				break;
 		}
 	}
@@ -244,7 +310,8 @@ int main (void) {
 	//Semaphores
 	ultraSem = osSemaphoreNew(1, 0, NULL);
 	
-	InitGPIO();
+	InitGPIOMotor();
+	initPIT();
 	initUART(BAUD_RATE);
 	osKernelInitialize();                 // Initialize CMSIS-RTOS
   osThreadNew(app_main, NULL, NULL);   
